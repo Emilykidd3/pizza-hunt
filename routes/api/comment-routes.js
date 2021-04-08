90869d0295f8ecb3202e93d28ed1c92c1708f352
emilykidd3 <e.kidd61@yahoo.com>
2021-04-08 13:30:29 -0700

    made responses to comments possible

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,18 @@
-const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const router = require("express").Router();
+const {
+  addComment,
+  addReply,
+  removeComment,
+  removeReply,
+} = require("../../controllers/comment-controller");
 
-// /api/comments/:pizzaId
-router.route('/:pizzaId').post(addComment);
+// /api/comments/:pizzaId add comment on a pizza
+router.route("/:pizzaId").post(addComment);
 
-// /api/comments/:commentId
-router.route('/:pizzaId/:commentId').delete(removeComment);
+// /api/comments/:pizzaId/:commentID add reply to a comment
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+
+// /api/comments/:pizzaId/:commentId/:replyID delete a reply to a comment
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 module.exports = router;
